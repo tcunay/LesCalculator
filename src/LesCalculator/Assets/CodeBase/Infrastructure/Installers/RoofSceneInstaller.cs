a6461503c4546dfb993c87f28cdee03b72f07f79
Gevorg <gulkanyangk@mail.ru>
2026-03-01 13:56:01 +0300

    Add RoofSceneInstaller.cs

---
+++ src/LesCalculator/Assets/CodeBase/Infrastructure/Installers/RoofSceneInstaller.cs
@@ -0,0 +1,12 @@
+using Zenject;
+
+namespace CodeBase.Infrastructure.Installers
+{
+    public class RoofSceneInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            
+        }
+    }
+}
